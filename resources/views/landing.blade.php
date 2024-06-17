1143bfeac10cb3754d05d53f348be88ffcbce191
daniram27 <daniramadhann@gmail.com>
2024-06-17 20:15:04 +0700

    update logo

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
-    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('pwa-512x512.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
     <meta name="theme-color" content="#ffffff">
     <title>{{ config('app.name') }} </title>
